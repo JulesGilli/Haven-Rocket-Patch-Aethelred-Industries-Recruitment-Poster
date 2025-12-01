c35dd1aad011feb2902e8156238f396fb30b0c86
JulesGilli <60229102+JulesGilli@users.noreply.github.com>
2025-12-01 16:24:20 +0100

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/julesgilli-haven-rocket-patch-aethelred-industries-recruitment-poster/',
+  base: '/Haven-Rocket-Patch-Aethelred-Industries-Recruitment-Poster/',
 })
