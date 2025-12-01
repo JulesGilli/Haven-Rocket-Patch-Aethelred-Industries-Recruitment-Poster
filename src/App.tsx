3993c455958c360b8f1d2236958fa718b4d50d91
JulesGilli <60229102+JulesGilli@users.noreply.github.com>
2025-12-01 16:31:39 +0100

    Update App component and add docs build output
    Added new build output files to the docs directory, including JavaScript, CSS, an image, and index.html. Updated src/App.tsx with new changes. package-lock.json was also updated, likely due to dependency changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
+
 import { LandingPage } from './pages/LandingPage';
 import { MissionPage } from './pages/MissionPage';
 import { ApplicationPage } from './pages/ApplicationPage';
+
 function ScrollToTop() {
-  const {
-    pathname
-  } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
 }
+
 export function App() {
-  return <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/mission" element={<MissionPage />} />
-        <Route path="/apply" element={<ApplicationPage />} />
-      </Routes>
-    </BrowserRouter>;
+    return (
+        <HashRouter>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/mission" element={<MissionPage />} />
+                <Route path="/apply" element={<ApplicationPage />} />
+            </Routes>
+        </HashRouter>
+    );
 }
